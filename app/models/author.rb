a3a9e793aefc7bff7a03fe8a887f86e52c3c0ae3
Will Lovell <willard.lovell@icloud.com>
2022-12-13 01:03:56 -0700

    commit tree 1618ae74c756631703392977b489057aa012cde9

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+validates :name, presence: true, allow_blank: false, uniqueness: { case_sensitive: true }
+validates :phone_number, length: { is: 10 }
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+	validates :title, presence: true, allow_blank: false
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+	validate  :true_facts
+
+	def true_facts
+		if title == "True Facts"
+			errors.add(:title, "Title is clickbait")
+		end
+
+	end
+
 end
